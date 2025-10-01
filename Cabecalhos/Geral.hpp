196aa46b879dfd42dab628b70d2659288a02aa7c
GabrielBressaglia <gabrielbressaglia@estudante.ufscar.br>
2025-10-01 20:14:19 -0300

    Delete Cabecalhos directory

--- Cabecalhos/Geral.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-unsigned int CELL_SIZE = 32;
-unsigned int CELL_CHUNCKS = 16;
-
-unsigned int MAPA_ALTURA = CELL_CHUNCKS * CELL_SIZE;
-unsigned int MAPA_LARGURA = CELL_CHUNCKS * CELL_SIZE;
-
-float VELOCIDADE_COMUM = 1;
